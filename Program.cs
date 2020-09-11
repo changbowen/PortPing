c8607e5e6eb7ac25989ef12e93da79fc77c0f2d0
Carl Chang <changb0wen@outlook.com>
2020-09-11 12:12:46 +0800

    adding source and udp option

--- Program.cs
+++ Program.cs
@@ -24,14 +24,29 @@ namespace PortPing
         static void Main(string[] args)
         {
             if (args?.Length < 2) {
-                Console.Write(UsageInfo);
+                Console.WriteLine(UsageInfo);
                 return;
             }
 
-            var host = args[0];
-            var port = int.Parse(args[1]);
-            var i = Array.IndexOf(args, @"-t");
-            var timeout = i == -1 ? 2000 : int.Parse(args[i + 1]);
+            // parse arguments
+            string arg_host, arg_source;
+            int arg_port, arg_timeout;
+            ProtocolType arg_protocol;
+
+            try {
+                arg_host = args[0];
+                arg_port = int.Parse(args[1]);
+                var i = Array.IndexOf(args, @"-t");
+                arg_timeout = i == -1 ? 2000 : int.Parse(args[i + 1]);
+                i = Array.IndexOf(args, @"-p");
+                arg_protocol = i == -1 ? ProtocolType.Tcp : (ProtocolType)Enum.Parse(typeof(ProtocolType), args[i + 1], true);
+                i = Array.IndexOf(args, @"-s");
+                arg_source = i == -1 ? null : args[i + 1];
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Invalid arguments.\r\n{getAllMessages(ex)}");
+                return;
+            }
 
             string formatResult(PingResult pingResult) =>
                 $@"Time: {pingResult.LatencyMs.ToString().PadLeft(timeout.ToString().Length)}ms; " +
@@ -41,7 +56,7 @@ namespace PortPing
                 $@"{(pingResult.Destination != null ? $@"To: {pingResult.Destination,21}; " : null)}";
 
             while (true) {
-                var result = CheckPort(host, port, timeout);
+                var result = CheckPort(arg_host, arg_port, arg_timeout);
                 if (result.Success)
                     Console.WriteLine($"Connection succeeded. {formatResult(result)}");
                 else
@@ -100,10 +115,12 @@ namespace PortPing
         }
 
         private const string UsageInfo = @"
-Usage: portping.exe host port [-t timeout]
-    host    The hostname or IP address to connect to.
-    port    Port number to connect to.
-    -t      Timeout in milliseconds to wait for each ping. Default is 2000ms.
+Usage: portping.exe host port [-t timeout] [-p protocol] [-s address]
+    host            The hostname or IP address to connect to.
+    port            Port number to connect to.
+    -t timeout      Timeout in milliseconds to wait for each ping. Default is 2000ms.
+    -p protocol     rotocol to use. Can be tcp or udp.
+    -s source       IP address of the source interface to use.
 ";
     }
 }
